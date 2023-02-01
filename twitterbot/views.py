f3c24d682edf539bee2ef76c07bce91e18da87e1
David Audu <davidaudu1010@gmail.com>
2023-02-01 23:34:47 +0100

    updated views and result page

--- twitterbot/views.py
+++ twitterbot/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import FormView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import AccountForm
+from django.contrib.auth import get_user_model
 from django.http import HttpResponseRedirect
 
 # Create your views here.
-def home(request):
-    if request.method == "POST":
-        form = AccountForm(request.POST)
-        if form.is_valid():
-            return HttpResponseRedirect("/")
-    else:
-        form = AccountForm()
-    return render(request, "index.html", {"form": form})
+# def home(request):
+#     if request.method == "POST":
+#         form = AccountForm(request.POST)
+#         user = get_user_model()
+#         print(user)
+#         if form.is_valid():
+#             return HttpResponseRedirect("/")
+#     else:
+#         form = AccountForm()
+#     return render(request, "index.html", {"form": form})
 
-# class HomePageView(LoginRequiredMixin, TemplateView):
-    form = AccountForm()
+class HomePageView(LoginRequiredMixin, FormView):
+    form_class = AccountForm
     template_name = "index.html"
     login_url = "account_login"
+    success_url = "/result/"
+
+class ResultView(LoginRequiredMixin, TemplateView):
+    template_name = "result.html"
+    

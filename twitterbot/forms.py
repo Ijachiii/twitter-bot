d1b5dadff2a45af3a9d68cd1495e7a9149d5274c
David Audu <davidaudu1010@gmail.com>
2023-02-04 12:49:53 +0100

    form placeholder

--- twitterbot/forms.py
+++ twitterbot/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class AccountForm(forms.Form):
-    username = forms.CharField(label="@username", max_length=300)
+    username = forms.CharField(label="", max_length=300, widget=forms.TextInput(attrs={"placeholder": "@username"}))

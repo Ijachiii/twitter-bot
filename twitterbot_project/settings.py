75f7002aff31986ba4997f5d488561e4294c553f
David Audu <davidaudu1010@gmail.com>
2023-02-10 20:17:22 +0100

    console mails

--- twitterbot_project/settings.py
+++ twitterbot_project/settings.py
@@ -152,15 +152,15 @@ AUTHENTICATION_BACKENDS = (
 )
 
 # sending mails
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
-EMAIL_HOST = env("EMAIL_HOST")
-EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS")
-EMAIL_PORT = env.int("EMAIL_PORT")
-EMAIL_USE_SSL = env.bool("EMAIL_USE_SSL")
+# DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
+# EMAIL_HOST = env("EMAIL_HOST")
+# EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+# EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS")
+# EMAIL_PORT = env.int("EMAIL_PORT")
+# EMAIL_USE_SSL = env.bool("EMAIL_USE_SSL")
 
 # allauth authentication
 ACCOUNT_EMAIL_REQUIRED = True

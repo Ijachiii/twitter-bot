7869a707080f84ba58c33c9b96c3c2681c1f33d7
David Audu <davidaudu1010@gmail.com>
2023-02-27 20:41:35 +0100

    migrations

--- twitterbot/admin.py
+++ twitterbot/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import TwitterAccountChecked
 
 # Register your models here.
-admin.site.register(TwitterAccountChecked)
+class TwitterAccountCheckedAdmin(admin.ModelAdmin):
+    model = TwitterAccountChecked
+    list_display = ["screen_name", "prediction"]
+
+admin.site.register(TwitterAccountChecked, TwitterAccountCheckedAdmin)

3cfd5a4a33f2e09762408d0cb571cef8abf61a39
David Audu <davidaudu1010@gmail.com>
2023-06-29 21:56:16 +0100

    url endpoimt for swagger

--- api/urls.py
+++ api/urls.py
@@ -11,9 +11,9 @@ SpectacularSwaggerView,
 urlpatterns = [
     path("", CheckAccountAPI.as_view(), name="check-account-api"),
     path("get-details/", UserDetailAPI.as_view()),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc",),
-    path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc",),
+    path("schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     # path("register/", RegisterUserAPI.as_view()),
     # path('api-token-auth/', views.obtain_auth_token, name='api-token-auth'), 
 ]
